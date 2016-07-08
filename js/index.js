e9ee0a65571288be7ee4d4bb3f60e6552d318bbf
chloeeffron <chloe.effron@gmail.com>
2016-07-07 21:16:29 -0400

    more edits
    more edits

--- js/index.js
+++ js/index.js
@@ -1,9 +1,26 @@
-$(function(){
+$(document).ready(function(){
+  
+  $('#slides').slick({
+    autoplay: true,
+    // adaptiveHeight: true,
+    arrows: false,
+    speed: 900,
+    fade: true,
+    cssEase: 'linear'
+  });
 
+   
+  $("#button").hover(buttonHover, buttonNormal);
 
+  function buttonHover() {
+    $("#button").css("backgroundColor", "#AAEB08");
+    $("#button").css("color", "black");
+ }
 
+  function buttonNormal() {
+    $("#button").css("backgroundColor", "black");
+    $("#button").css("color", "white");
+  }
 
-
-
-
+  
 });
